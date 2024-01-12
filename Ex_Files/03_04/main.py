945b5c7f740650a74044562c4ebc28c8d8643b80
Ethan W <56050298+ewhit004@users.noreply.github.com>
2024-01-12 18:48:57 +0000

    Filtering Output Challenge Solution

--- Ex_Files/03_04/main.py
+++ Ex_Files/03_04/main.py
@@ -26,8 +26,12 @@ with open("laureates.csv", "r") as f:
 # 2. You can add to a list using
 #      my_list.append("something")
 
+#defining the dictionary creating the json file
 laureates_beginning_with_a = []
-# LinkedIn learner code here
+#LinkedIn learner code here
+for laureate in laureates:
+    if laureate['name'][0] == "A":
+        laureates_beginning_with_a.append(laureate)
 
 
 with open("laureates.json", "w") as f:
