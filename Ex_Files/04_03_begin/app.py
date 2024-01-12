ca9150d4b74bd6d093d5df5de0c4685936cce56b
Ethan W <56050298+ewhit004@users.noreply.github.com>
2024-01-12 19:39:37 +0000

    Adding Search Functionality to Web Server Containing Laureate Data

--- Ex_Files/04_03_begin/app.py
+++ Ex_Files/04_03_begin/app.py
@@ -18,12 +18,22 @@ def index():
 @app.route("/laureates/")
 def laureate_list():
     # template found in templates/laureate.html
+    
+    #empty list of laureates
     results = []
     if not request.args.get("surname"):
         return jsonify(results)
 
-    # Your code here!
-    return "your code here!"
+    #if a surname param is available, then use the search_string variable
+    #lower() ensures a looser match and strip() cleans up the search
+    search_string = request.args.get("surname").lower().strip()
+
+    #run through each laureate
+    for laureate in laureates:
+        #checking if search_string is inside of laureates lowercase surname
+        if search_string in laureate["surname"].lower():
+            results.append(laureate)
+    return jsonify(results)
 
 
 app.run(debug=True)
