4f8dfd04bde88b309382aff29dd42c01a44f4821
Ethan W <56050298+ewhit004@users.noreply.github.com>
2024-01-12 19:09:20 +0000

    Practicing w/ Requests Python Package

--- Ex_Files/04_01_begin/main.py
+++ Ex_Files/04_01_begin/main.py
@@ -1,6 +1,18 @@
 import requests
+#if package is not installed type "pip install package_name" OR if 
+#you're not using codespaces you may need to use the following command
+#"python3 -m pip install package_name"
 
+#".get(http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json)"
+#takes data from a World Bank population API from a website
 response = requests.get(
     "http://api.worldbank.org/v2/countries/USA/indicators/SP.POP.TOTL?per_page=5000&format=json")
 
+#20 items pulled from above API; the most recent 20 years are pulled
 last_twenty_years = response.json()[1][:20]
+
+#visual representation of 20 items (years & values) pulled denoted by "="
+for year in last_twenty_years:
+  display_width = year["value"] // 10_000_000
+  #year with growing "=" indicating a growing population
+  print(f'{year["date"]}: {year["value"]}', "=" * display_width)
